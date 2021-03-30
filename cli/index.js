80a4cb4a9e0fdb4c5de7ac7cf35b1fe1e0b92a55
yosiopp <yosiopp.develop@gmail.com>
2021-03-30 12:36:42 +0900

    jsonschemaのライブラリをajvに変更 cli処理の全体の流れを記載

--- cli/index.js
+++ cli/index.js
@@ -1,3 +1,75 @@
 #!/usr/bin/env node
+const fs = require('fs');
+const openerd = require('../index');
 
-console.log('hello openerd');
+let procno = 0;
+
+(async () => {
+  try {
+    // 起動パラメタのパース -> options
+    procno = 1;
+    const [options, context] = parseArguments(process.argv);
+    if (context) {
+      console.log(context.output);
+      if (context.exit) {
+        return;
+      }
+    }
+
+    // 入力の取得
+    procno = 2;
+    const input = getInput(options);
+    
+    // yamlの検証
+    procno = 3;
+    const [ret, data] = openerd.validate(input);
+    if (!ret) {
+      // 検証エラー
+      throw { message: 'validate error', data };
+    }
+    
+    // render
+    procno = 4;
+    const output = openerd.render(data, options);
+
+    // 出力
+    procno = 5;
+    if (options.output === 'file') {
+      // file出力
+    } else {
+      // 標準出力
+      console.log(output);
+    }
+    
+  } catch(e) {
+    console.error(procno, e);
+    process.exit(procno);
+  }
+})();
+
+function parseArguments(argv) {
+  const options = {
+    input: 'stdin',
+    output: 'stdout',
+    format: 'svg',
+  };
+  if (argv.length > 2) {
+    // オプション処理
+  }
+  return [options, null];
+}
+
+function getInput(options) {
+  return `
+openerd: 1.0.0
+info:
+  title: test
+`;
+
+  if (options.input === 'stdin' || options.input === '-') {
+    // TODO 標準入力
+  } else {
+    // ファイル入力
+    return fs.readFileSync(options.input, { encoding: 'utf-8' });
+  }
+}
---
+++ index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const Yaml = require('yaml');
+const Ajv = require("ajv");
+const svg = require('./lib/svg');
+const ddl = require('./lib/ddl');
+
+exports.validate = (input) => {
+  const data = Yaml.parse(input);
+  const schema = Yaml.parse(fs.readFileSync("schema.yaml", { encoding: 'utf-8' }));
+  const ajv = new Ajv(); 
+  const validate = ajv.compile(schema);
+  const valid = validate(data);
+  if (!valid) {
+    console.error(2, validate.errors);
+  }
+  return [valid, valid ? data : validate.errors];
+}
+
+exports.render = (data, options) => {
+  if (options.format === 'svg') {
+    return svg.render(data, options);
+  } else if (options.format === 'ddl') {
+    return ddl.render(data, options);
+  } else {
+    throw `unknown format: ${options.format}`;
+  }
+}
